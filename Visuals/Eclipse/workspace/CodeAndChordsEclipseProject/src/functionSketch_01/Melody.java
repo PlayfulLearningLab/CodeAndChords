a51913f1343847b4110f6b30df34707ca8baa12c
Emily Meuer <emily.meuer@gmail.com>
2017-06-13 09:39:56 -0500

    Corrected rhythms of the melodies

--- Visuals/Eclipse/workspace/CodeAndChordsEclipseProject/src/functionSketch_01/Melody.java
+++ Visuals/Eclipse/workspace/CodeAndChordsEclipseProject/src/functionSketch_01/Melody.java
@@ -14,12 +14,12 @@ import processing.core.PApplet;
  *
  */
 public class Melody {
-	
+
 	private static HashMap<String, float[][]>	melodyLib  = Melody.initializeLib();
 	private	static String[]						keys	= new String[] { 
 			" A "," A# / Bb "," B ", " C ", " C# / Db ", " D ",
 			" D# / Eb ", " E ", " F ", " F# / Gb "," G "," G# / Ab ", 
-			};
+	};
 	private LinkedList<Note>                    mel;
 	private float                               highRange;
 	private float                               lowRange;
@@ -33,7 +33,7 @@ public class Melody {
 	{
 		this.parent	= parent;
 	}
-	
+
 	/**
 	 * Where most of the work of playing the Melody is done.
 	 * 
@@ -46,12 +46,12 @@ public class Melody {
 	public void playMelody(String key, float bpm, String scale, int rangeOctave, Instrument instrument)
 	{
 		float[][]	curMelody	= melodyLib.get(scale.trim().toLowerCase());
-//		int			scaleLength	= curMelody.length;
-//		int[]		midiNotes	= new int[scaleLength];
-//		float[]		durations	= new float[scaleLength];
+		//		int			scaleLength	= curMelody.length;
+		//		int[]		midiNotes	= new int[scaleLength];
+		//		float[]		durations	= new float[scaleLength];
 		Note[]		notes		= new Note[curMelody.length];
-		float		defaultAmp	= 100;
-		
+		float		defaultAmp	= 40;
+
 		// Find position of notes in this key:
 		int		keyPos		= -1;
 		for(int i = 0; i < Melody.keys.length; i++)
@@ -65,15 +65,15 @@ public class Melody {
 		{
 			throw new IllegalArgumentException("Melody.playMelody: key " + key + " is not a valid key.");
 		} // error checking
-		
+
 		// Calculate milli's per Note, given the tempo:
 		float quarterNoteTime = (float) (Math.pow(bpm, -1) * 60 * 1000);
-		
+
 		int		scalePos;
 		int		midiNote;
 		float	duration;
 		float		amplitude;
-		
+
 		// Calculate midi notes, given the key and range:
 		for(int i = 0; i < curMelody.length; i++)
 		{
@@ -82,19 +82,19 @@ public class Melody {
 			{
 				scalePos	= (int) ((curMelody[i][0]) + keyPos);
 				midiNote	= (12 * rangeOctave) + scalePos;
-				
+
 				duration	= curMelody[i][1] * quarterNoteTime;
 				amplitude	= defaultAmp;
 			} else {
 				// but if not, use -1 as a placeholder:
 				midiNote	= -1;
-				duration	= 1000;
+				duration	= 1 * quarterNoteTime;
 				amplitude	= 0;
 			}
 
 			// create an array of Notes:
 			notes[i]	= new Note(midiNote, duration, amplitude);
-			
+
 		} // for - calculate midi notes
 
 		float	nextNoteStartTime	= parent.millis();
@@ -102,27 +102,27 @@ public class Melody {
 		for(int i = 0; i < notes.length; i++)
 		{
 			while(parent.millis() < nextNoteStartTime)	{ }
-			
+
 			instrument.playNote(notes[i]);
-			
+
 			nextNoteStartTime	= parent.millis() + notes[i].getDuration();
 		} // for - play Notes
-		
+
 	} // playMelody
-	
+
 	/*	
 	 * TODO: get rid of these?
 	public float getHighRange()
 	{
 		return this.highRange;
 	}
-	
+
 	public float getLowRange()
 	{
 		return this.lowRange;
 	}
-	*/
-	
+	 */
+
 	/**
 	 * Fills a HashMap with float[]'s of the scale degrees, durations, and rests
 	 * that make up the chromatic, major and minor melodies.
@@ -132,235 +132,165 @@ public class Melody {
 	private static HashMap<String, float[][]> initializeLib()
 	{
 		HashMap<String, float[][]> hm = new HashMap<String, float[][]>();
-		
+
 		hm.put("chromatic", new float[][]
 				{ 
-						new float[] { 0,1 },  // 0:1
-						new float[] { },  
-						new float[] { 1,1 },  // 0:2
-						new float[] {  },  
-						new float[] { 2,1 },  // 0:3
-						new float[] {  },  
-						new float[] { 3,1 },  // 0:4
-						new float[] {  }, 
-						new float[] { 4,1 },  // 1:1
-						new float[] {  },  
-						new float[] { 5,1 },  // 1:2
-						new float[] {  },  
-						new float[] { 6,1 },  // 1:3
-						new float[] {  },  
-						new float[] { 7,1 },  // 1:4
-						new float[] {  },  
-						new float[] { 8,1 },  // 2:1
-						new float[] {  },  
-						new float[] { 9,1 },  // 2:2
-						new float[] {  },  
-						new float[] { 10,1 },  // 2:3
-						new float[] {  },  
-						new float[] { 11,1 },  // 2:4
-						new float[] {  },  
-						new float[] { 12,2 },  // 3:1
-						new float[] {  },
-						new float[] {  },
-						new float[] {  },
-						new float[] { 12, (float) 0.25 },
-						new float[] { 11, (float) 0.25 },  // 3:2
-						new float[] { 10, (float) 0.25 },  
-						new float[] { 9, (float) 0.25 },  // 3:3
-						new float[] { 8, (float) 0.25 },  
-						new float[] { 7, (float) 0.25 },  // 3:4
-						new float[] { 6, (float) 0.25 },  
-						new float[] { 5, (float) 0.25 },  // 4:1
-						new float[] { 4, (float) 0.25 },  
-						new float[] { 3, (float) 0.25 },  // 4:2
-						new float[] { 2, (float) 0.25 },  
-						new float[] { 1, (float) 0.25 },  // 4:3
-						new float[] { 0, 1 },  
-						new float[] {  },  // 4:4
-						new float[] {  },  
-						new float[] {  },  // 5:1
-						new float[] {  },  
-						new float[] {  },  // 5:2
-						new float[] {  },  
-						new float[] {  },  // 5:3
-						new float[] {  },  
-						new float[] {  },  // 5:4
-						new float[] {  }
-					}  );
-		
+			new float[] { 0,1 },  // 0:1
+			new float[] { 1,1 },  // 0:2
+			new float[] { 2,1 },  // 0:3
+			new float[] { 3,1 },  // 0:4
+			new float[] { 4,1 },  // 1:1
+			new float[] { 5,1 },  // 1:2
+			new float[] { 6,1 },  // 1:3
+			new float[] { 7,1 },  // 1:4
+			new float[] { 8,1 },  // 2:1
+			new float[] { 9,1 },  // 2:2
+			new float[] { 10,1 },  // 2:3
+			new float[] { 11,1 },  // 2:4
+			new float[] { 12,2 },  // 3:1
+			new float[] {  },	// 3:2
+			new float[] { 12, (float) 0.5 },	// 3:3
+			new float[] { 11, (float) 0.5 },  
+			new float[] { 10, (float) 0.5 },	// 3:4
+			new float[] { 9, (float) 0.5 },  
+			new float[] { 8, (float) 0.5 },  // 4:1
+			new float[] { 7, (float) 0.5 },
+			new float[] { 6, (float) 0.5 },  // 4:2
+			new float[] { 5, (float) 0.5 },
+			new float[] { 4, (float) 0.5 },  // 4:3
+			new float[] { 3, (float) 0.5 },  
+			new float[] { 2, (float) 0.5 },  // 4:4
+			new float[] { 1, (float) 0.5 },  
+			new float[] { 0, 1 },  			// 5:1
+			new float[] {  },
+				}  );
+
 		hm.put("major", new float[][]
 				{ 
-						new float[] { 0,1 },  // 0:1
-						new float[] {  },  
-						new float[] { 2,1 },  // 0:2
-						new float[] {  },  
-						new float[] { 4,1 },  // 0:3
-						new float[] {  },  
-						new float[] { 5,1 },  // 0:4
-						new float[] {  }, 
-						new float[] {  },  // 1:1
-						new float[] {  },  
-						new float[] { 7,1 },  // 1:2
-						new float[] {  },  
-						new float[] {  },  // 1:3
-						new float[] {  },  
-						new float[] { 9,1 },  // 1:4
-						new float[] {  },  
-						new float[] { 11,1 },  // 2:1
-						new float[] {  },  
-						new float[] { 12,1 },  // 2:2
-						new float[] {  },  
-						new float[] {  },  // 2:3
-						new float[] {  },  
-						new float[] { 12,1 },  // 2:4
-						new float[] {  },  
-						new float[] { 11,1 },  // 3:1
-						new float[] {  },  
-						new float[] {  },  // 3:2
-						new float[] {  },  
-						new float[] { 9, (float) 0.5 },  // 3:3
-						new float[] { 7, (float) 0.5 },  
-						new float[] { 5, (float) 0.5 },  // 3:4
-						new float[] { 4, (float) 0.5 },  
-						new float[] { 2, 1 },  // 4:1
-						new float[] {  },  
-						new float[] { 0,1 },  // 4:2
-						new float[] {  },  
-						new float[] {  },  // 4:3
-						new float[] {  },  
-						new float[] { 0,1 },  // 4:4
-						new float[] {  },  
-						new float[] { -7, 1 },  // 5:1
-						new float[] {  },  
-						new float[] { 0,1 },  // 5:2
-						new float[] {  },  
-						new float[] { -5,1 },  // 5:3
-						new float[] {  },  
-						new float[] { 0,1 },  // 5:4
-						new float[] {  },
-						new float[] { 0, (float) 0.5 },  // 6:1
-						new float[] { 2, (float) 0.5 },  
-						new float[] { 4, (float) 0.5 },  // 6:2
-						new float[] { 5, (float) 0.5 },  
-						new float[] { 7, (float) 0.5 },  // 6:3
-						new float[] { 9, (float) 0.5 },  
-						new float[] { 11,(float) 0.5 },  // 6:4
-						new float[] { 12, (float) 0.5 },
-						new float[] { 11, (float) 0.5 },  // 7:1
-						new float[] { 9, (float) 0.5 },  
-						new float[] { 7, (float) 0.5 },  // 7:2
-						new float[] { 5, (float) 0.5 },  
-						new float[] { 4, (float) 0.5 },  // 7:3
-						new float[] { 2, (float) 0.5 },  
-						new float[] { 0, 1 },  // 7:4
-						new float[] {  },
-					} );
-		
+			new float[] { 0,1 },  // 0:1
+			new float[] { 2,1 },  // 0:2
+			new float[] { 4,1 },  // 0:3
+			new float[] { 5,1 },  // 0:4
+			new float[] {  },  // 1:1
+			new float[] { 7,1 },  // 1:2
+			new float[] {  },  // 1:3
+			new float[] { 9,1 },  // 1:4
+			new float[] { 11,1 },  // 2:1
+			new float[] { 12,1 },  // 2:2
+			new float[] {  },  // 2:3
+			new float[] { 12,1 },  // 2:4
+			new float[] { 11,1 },  // 3:1
+			new float[] {  },  // 3:2
+			new float[] { 9, (float) 0.5 },  // 3:3
+			new float[] { 7, (float) 0.5 },  
+			new float[] { 5, (float) 0.5 },  // 3:4
+			new float[] { 4, (float) 0.5 },  
+			new float[] { 2, 1 },	// 4:1
+			new float[] { 0,1 },	// 4:2
+			new float[] {  },		// 4:3
+			new float[] {  },		// 4:4
+			new float[] { 0,1 },	// 5:1
+			new float[] { -7, 1 },	// 5:2
+			new float[] { 0,1 },	// 5:3 
+			new float[] { -5,1 },	// 5:4
+			new float[] { 0,1 },	// 6:1
+			new float[] { 0, (float) 0.5 },	// 6:2
+			new float[] { 2, (float) 0.5 },  
+			new float[] { 4, (float) 0.5 },	// 6:3
+			new float[] { 5, (float) 0.5 },  
+			new float[] { 7, (float) 0.5 },	// 6:4
+			new float[] { 9, (float) 0.5 },  
+			new float[] { 11,(float) 0.5 },	// 7:1
+			new float[] { 12, (float) 0.5 },
+			new float[] { 11, (float) 0.5 },	// 7:2
+			new float[] { 9, (float) 0.5 },  
+			new float[] { 7, (float) 0.5 },  // 7:3
+			new float[] { 5, (float) 0.5 },  
+			new float[] { 4, (float) 0.5 },  // 7:4
+			new float[] { 2, (float) 0.5 },  
+			new float[] { 0, 1 },  // 8:1
+			new float[] {  },
+				} );
+
 		hm.put("minor", new float[][]
 				{ 
-						new float[] { 0,1 },  // 0:1
-						new float[] {  },  
-						new float[] { 2,1 },  // 0:2
-						new float[] {  },  
-						new float[] { 3,1 },  // 0:3
-						new float[] {  },  
-						new float[] { 5,1 },  // 0:4
-						new float[] {  }, 
-						new float[] {  },  // 1:1
-						new float[] {  },  
-						new float[] { 7,1 },  // 1:2
-						new float[] {  },  
-						new float[] {  },  // 1:3
-						new float[] {  },  
-						new float[] { 8,1 },  // 1:4
-						new float[] {  },  
-						new float[] { 10,1 },  // 2:1
-						new float[] {  },  
-						new float[] { 12,1 },  // 2:2
-						new float[] {  },  
-						new float[] {  },  // 2:3
-						new float[] {  },  
-						new float[] { 12,1 },  // 2:4
-						new float[] {  },  
-						new float[] { 10,1 },  // 3:1
-						new float[] {  },  
-						new float[] {  },  // 3:2
-						new float[] {  },  
-						new float[] { 8, (float) 0.5 },  // 3:3
-						new float[] { 7, (float) 0.5 },  
-						new float[] { 5, (float) 0.5 },  // 3:4
-						new float[] { 3, (float) 0.5 },  
-						new float[] { 2,1 },  // 4:1
-						new float[] {  },  
-						new float[] { 0,1 },  // 4:2
-						new float[] {  },  
-						new float[] {  },  // 4:3
-						new float[] {  },  
-						new float[] { 0,1 },  // 4:4
-						new float[] {  },  
-						new float[] { -7,1 },  // 5:1
-						new float[] {  },  
-						new float[] { 0,1 },  // 5:2
-						new float[] {  },  
-						new float[] { -5,1 },  // 5:3
-						new float[] {  },  
-						new float[] { 0,1 },  // 5:4
-						new float[] {  },
-						new float[] { 0,(float) 0.25 },  // 6:1
-						new float[] { 2,(float) 0.25 },  
-						new float[] { 3,(float) 0.25 },  // 6:2
-						new float[] { 5,(float) 0.25 },  
-						new float[] { 7,(float) 0.25 },  // 6:3
-						new float[] { 8,(float) 0.25 },  
-						new float[] { 10,(float) 0.25 },  // 6:4
-						new float[] { 12,(float) 0.25 },
-						new float[] { 10,(float) 0.25 },  // 7:1
-						new float[] { 8,(float) 0.25 },  
-						new float[] { 7,(float) 0.25 },  // 7:2
-						new float[] { 5,(float) 0.25 },  
-						new float[] { 3,(float) 0.25 },  // 7:3
-						new float[] { 2,(float) 0.25 },  
-						new float[] { 0,(float) 0.25 },  // 7:4
-						new float[] {  },
-						new float[] { 0,(float) 0.5 },  // 6:1
-						new float[] { 2,(float) 0.5 },  
-						new float[] { 3,(float) 0.5 },  // 6:2
-						new float[] { 5,(float) 0.5 },  
-						new float[] { 7,(float) 0.5 },  // 6:3
-						new float[] { 8,(float) 0.5 },  
-						new float[] { 11,(float) 0.5 },  // 6:4
-						new float[] { 12,(float) 0.5 },
-						new float[] { 11,(float) 0.5 },  // 7:1
-						new float[] { 8,(float) 0.5 },  
-						new float[] { 7,(float) 0.5 },  // 7:2
-						new float[] { 5,(float) 0.5 },  
-						new float[] { 3,(float) 0.5 },  // 7:3
-						new float[] { 2,(float) 0.5 },  
-						new float[] { 0,(float) 0.5 },  // 8:1
-						new float[] {  },  
-						new float[] {  },
-						new float[] { 0, (float) 0.5 },  // 6:1
-						new float[] { 2, (float) 0.5 },  
-						new float[] { 3,(float) 0.5 },  // 6:2
-						new float[] { 5,(float) 0.5 },  
-						new float[] { 7,(float) 0.5 },  // 6:3
-						new float[] { 9,(float) 0.5 },  
-						new float[] { 11,(float) 0.5 },  // 6:4
-						new float[] { 12,(float) 0.5 },
-						new float[] { 10,(float) 0.5 },  // 7:1
-						new float[] { 8,(float) 0.5 },  
-						new float[] { 7,(float) 0.5 },  // 7:2
-						new float[] { 5,(float) 0.5 },  
-						new float[] { 3,(float) 0.5 },  // 7:3
-						new float[] { 2,(float) 0.5 },  
-						new float[] { 0,(float) 0.5 },// 8:2
-						new float[] {  },  
-						new float[] {  },  // 8:3
-						new float[] {  },  
-						new float[] {  },  // 8:4
-						new float[] {  }
+			new float[] { 0,1 },  // 0:1
+			new float[] { 2,1 },  // 0:2
+			new float[] { 3,1 },  // 0:3
+			new float[] { 5,1 },  // 0:4
+			new float[] {  },  // 1:1
+			new float[] { 7,1 },  // 1:2
+			new float[] {  },  // 1:3
+			new float[] { 8,1 },  // 1:4
+			new float[] { 10,1 },  // 2:1
+			new float[] { 12,1 },  // 2:2
+			new float[] {  },  // 2:3
+			new float[] { 12,1 },  // 2:4
+			new float[] { 10,1 },  // 3:1
+			new float[] {  },  // 3:2
+			new float[] { 8, (float) 0.5 },  // 3:3
+			new float[] { 7, (float) 0.5 },  
+			new float[] { 5, (float) 0.5 },  // 3:4
+			new float[] { 3, (float) 0.5 },  
+			new float[] { 2, 1 },	// 4:1
+			new float[] { 0,1 },	// 4:2
+			new float[] {  },		// 4:3
+			new float[] { 0,1 },	// 
+			new float[] { -7, 1 },	// 5:1
+			new float[] { 0,1 },	// 5:2
+			new float[] { -5,1 },	// 5:3 
+			new float[] { 0,1 },	// 5:4
+
+			new float[] { 0,(float) 0.5 },  // 6:1
+			new float[] { 2,(float) 0.5 },  
+			new float[] { 3,(float) 0.5 },  // 6:2
+			new float[] { 5,(float) 0.5 },  
+			new float[] { 7,(float) 0.5 },  // 6:3
+			new float[] { 8,(float) 0.5 },  
+			new float[] { 10,(float) 0.5 },  // 6:4
+			new float[] { 12,(float) 0.5 },
+			new float[] { 10,(float) 0.5 },  // 7:1
+			new float[] { 8,(float) 0.5 },  
+			new float[] { 7,(float) 0.5 },  // 7:2
+			new float[] { 5,(float) 0.5 },  
+			new float[] { 3,(float) 0.5 },  // 7:3
+			new float[] { 2,(float) 0.5 },  
+			new float[] { 0,(float) 1 },  // 7:4
+
+			new float[] { 0,(float) 0.5 },  // 8:1
+			new float[] { 2,(float) 0.5 },  
+			new float[] { 3,(float) 0.5 },  // 8:2
+			new float[] { 5,(float) 0.5 },  
+			new float[] { 7,(float) 0.5 },  // 8:3
+			new float[] { 8,(float) 0.5 },  
+			new float[] { 11,(float) 0.5 },  // 8:4
+			new float[] { 12,(float) 0.5 },
+			new float[] { 11,(float) 0.5 },  // 9:1
+			new float[] { 8,(float) 0.5 },  
+			new float[] { 7,(float) 0.5 },  // 9:2
+			new float[] { 5,(float) 0.5 },  
+			new float[] { 3,(float) 0.5 },  // 9:3
+			new float[] { 2,(float) 0.5 },  
+			new float[] { 0,(float) 0.5 },  // 9:4
+			new float[] {  },  
+			new float[] { 0, (float) 0.5 },  // 10:1
+			new float[] { 2, (float) 0.5 },  
+			new float[] { 3,(float) 0.5 },  // 10:2
+			new float[] { 5,(float) 0.5 },  
+			new float[] { 7,(float) 0.5 },  // 10:3
+			new float[] { 9,(float) 0.5 },  
+			new float[] { 11,(float) 0.5 },  // 10:4
+			new float[] { 12,(float) 0.5 },
+			new float[] { 10,(float) 0.5 },  // 11:1
+			new float[] { 8,(float) 0.5 },  
+			new float[] { 7,(float) 0.5 },  // 11:2
+			new float[] { 5,(float) 0.5 },  
+			new float[] { 3,(float) 0.5 },  // 11:3
+			new float[] { 2,(float) 0.5 },  
+			new float[] { 0,(float) 1 },// 11:4
+			new float[] {  },
 				} );
-		
+
 		return hm;
 	} // initializeLib
 } // Melody
