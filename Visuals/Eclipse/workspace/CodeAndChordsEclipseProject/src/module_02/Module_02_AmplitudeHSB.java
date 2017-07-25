3ea85d7f7473f64b0893a09fd5b3d668e24999ef
Emily Meuer <emily.meuer@gmail.com>
2017-07-25 16:29:09 -0500

    Comments on ModuleTemplate_01 and ModuleTemplate

--- Visuals/Eclipse/workspace/CodeAndChordsEclipseProject/src/module_02/Module_02_AmplitudeHSB.java
+++ Visuals/Eclipse/workspace/CodeAndChordsEclipseProject/src/module_02/Module_02_AmplitudeHSB.java
@@ -40,7 +40,7 @@ public class Module_02_AmplitudeHSB extends PApplet {
 		}; // thresholds
 
 		// Define them some default colors
-		this.moduleTemplate.setColors(new float[][] {
+		this.moduleTemplate.colors = new float[][] {
 			new float[] { 255, 0, 0 },
 			new float[] { 0, 255, 0 },
 			new float[] { 0, 0, 255 },
