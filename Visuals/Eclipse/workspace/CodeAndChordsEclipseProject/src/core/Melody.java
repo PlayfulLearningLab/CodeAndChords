7e6bb578067b8ef8e654f82493c9b9496a566e73
Emily Meuer <emily.meuer@gmail.com>
2017-08-24 11:35:59 -0500

    Prep to merge in D's Module_02 amplitude guide tones

--- Visuals/Eclipse/workspace/CodeAndChordsEclipseProject/src/core/Melody.java
+++ Visuals/Eclipse/workspace/CodeAndChordsEclipseProject/src/core/Melody.java
@@ -229,7 +229,6 @@ public class Melody implements Runnable {
 			{
 				instrument.playNote(notes[i]);
 
-
 				if(this.input != null)
 				{
 					this.input.setFundamentalArray(new float[] { Pitch.mtof(notes[i].getMidiNum()) });
