7d57e24336ec30a064a844de28964819db28781d
danmahota <danmahota@gmail.com>
2018-04-11 15:41:00 -0500

    here class

--- Visuals/Eclipse/workspace/CodeAndChordsEclipseProject/src/cadenza/Here.java
+++ Visuals/Eclipse/workspace/CodeAndChordsEclipseProject/src/cadenza/Here.java
@@ -1,6 +1,81 @@
 package cadenza;
 
-public class Here 
+import core.Module;
+import core.ModuleMenu;
+import core.input.RealTimeInput;
+import net.beadsproject.beads.core.AudioContext;
+import processing.core.PApplet;
+
+public class Here extends Module
 {
+	public static void main(String[] args)
+	{
+		PApplet.main("module_01_02.Module_01_02_PitchHue_MultipleInputs");
+	} // main
+
+	public void setup() 
+	{
+		this.input	= new RealTimeInput(16, new AudioContext(), true, this);
+		//this.input          = RealTimeInput() Elena give this the audiocontext in branch
+		this.totalNumInputs	= this.input.getAdjustedNumInputs();
+		this.curNumInputs	= 2;
+
+		this.menu	= new ModuleMenu(this, this, this.input, 12);
+
+		this.setSquareValues();
+
+		// call add methods:
+		this.menu.addLandingMenu();
+		this.menu.addSensitivityMenu(true);
+		this.menu.addColorMenu();
+	} // setup()
+
+
+	public void draw()
+	{
+		int	scaleDegree;
+
+		// The following line is necessary so that key press shows the menu button
+		/*		if (keyPressed == true && !this.menu.getIsRunning()) 
+		{
+			this.menu.setMenuVal();
+		} // if keyPressed
+		 */
+
+		for(int i = 0; i < this.curNumInputs; i++)
+		{
+			//			System.out.println("input.getAdjustedFundAsMidiNote(" + (i + 1) + ") = " + input.getAdjustedFundAsMidiNote(i + 1) + 
+			//					"; input.getAmplitude(" + (i + 1) + ") = " + input.getAmplitude(1 + 1));
+
+			scaleDegree	= (round(input.getAdjustedFundAsMidiNote(i + 1)) - this.menu.getCurKeyEnharmonicOffset() + 3 + 12) % 12;
+
+			this.menu.universalFade(scaleDegree);
+
+			this.fill(this.menu.getCurHue()[i][0], this.menu.getCurHue()[i][1], this.menu.getCurHue()[i][2]);
+
+			int	curX;
+			int	curY;
+
+			curX	= (int)this.menu.mapCurrentXPos(this.xVals[i]);
+			curY	= (int)this.menu.mapCurrentYPos(this.yVals[i]);
+			this.rect(curX, curY, this.rectWidths[i], this.rectHeights[i]);
+
+			if(this.menu.isShowScale())
+			{
+				this.legend(scaleDegree, i);
+			}
+
+		} // for
+
+		this.menu.runMenu();
+
+	} // draw()
+
+
+
+	public String[] getLegendText()
+	{
+		return this.menu.getScale(this.menu.getCurKey(), this.menu.getMajMinChrom());
+	} // getLegendText
 
 }
