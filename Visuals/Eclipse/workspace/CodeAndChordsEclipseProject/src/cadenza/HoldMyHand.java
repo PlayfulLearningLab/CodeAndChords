1e40b2a94b9901232f26d2ea48ba9cdb03185f4a
Emily Meuer <emily.meuer@gmail.com>
2018-04-15 15:53:36 -0500

    Here tracks

--- Visuals/Eclipse/workspace/CodeAndChordsEclipseProject/src/cadenza/HoldMyHand.java
+++ Visuals/Eclipse/workspace/CodeAndChordsEclipseProject/src/cadenza/HoldMyHand.java
@@ -9,12 +9,16 @@ package cadenza;
 import core.Module;
 import core.ModuleMenu;
 import processing.core.PApplet;
-import core.Shape;
-import core.ShapeEditor;
 import core.input.RealTimeInput;
 import net.beadsproject.beads.core.AudioContext;
 
 public class HoldMyHand extends Module /*implements ShapeEditorInterface */{
+	int	soloistInput	= 0;
+	
+	int[]	inputNums	= {
+			1, 2, 3
+	};
+	
 	int xspacing; 
 	int yspacing;
 	int w; 
@@ -52,22 +56,33 @@ public class HoldMyHand extends Module /*implements ShapeEditorInterface */{
 		//		this.input	= new Input(this);
 		 w = width;
 		 h = height;
+		 
 		 xspacing = width/70;
 		 yspacing = height/70;
-		 maxAmp=40;
+		 
+//		 maxAmp=40;
+		 maxAmp=400;
+		 
 		 dx = (TWO_PI / period) * xspacing;
+		 
 		 yvalues = new float[h/yspacing];
-		 for(int i=0; i<yspacing;i++)
+		 for(int i=0; i<yvalues.length;i++)
 		 {
 			 yvalues[i]=h-(i*yspacing);
 		 }
 
-		this.input    = new RealTimeInput(1, new AudioContext(), this);
+		this.input    = new RealTimeInput(7, new AudioContext(), this);
 		this.totalNumInputs = this.input.getAdjustedNumInputs();
-		this.curNumInputs = 4;
+//		this.curNumInputs = 4;
 
 		this.menu	= new ModuleMenu(this, this, this.input, 12);
+		this.curNumInputs	= this.menu.getRecInput().getNumInputs();
+		
+		this.menu.setCurKey("G", 2);
 
+		this.inputWidth = width/this.inputNums.length; 
+		this.numEllipses  = inputWidth/xspacing;
+		
 		this.yVals		= new int[18];
 		// Seemed like a good starting position, related to the text - but pretty arbitrary:
 		this.yVals[0]	= 50;
@@ -82,7 +97,8 @@ public class HoldMyHand extends Module /*implements ShapeEditorInterface */{
 				"Canvas", "1", "2", "3", "4", "5", "6"
 		};
 
-		this.menu.addColorMenu(buttonLabels, 1, true, null, false, false, "Dynamic\nSegments", 6, 6, "color");
+//		this.menu.addColorMenu(buttonLabels, 1, true, null, false, false, "Dynamic\nSegments", 6, 6, "color");
+		this.menu.addColorMenu();
 		this.menu.addSensitivityMenu(false);
 
 		this.menu.addShapeSizeSlider(0, this.yVals[15]);
@@ -94,6 +110,14 @@ public class HoldMyHand extends Module /*implements ShapeEditorInterface */{
 		this.menu.setBPM(30);
 
 		//				this.textSize(32);
+		
+		this.menu.getControlP5().getController("trichrom").update();
+
+		this.menu.setColor(0, new int[] { 254, 183, 78 }, true);
+		this.menu.setColor(4, new int[] { 0, 0, 200 }, true);
+		this.menu.setColor(8, new int[] { 150, 0, 150 }, true);
+
+		this.menu.getControlP5().getController("trichrom").update();
 
 	} // setup
 
@@ -102,8 +126,6 @@ public class HoldMyHand extends Module /*implements ShapeEditorInterface */{
 		int	scaleDegree;
 
 		// The following line is necessary so that key press shows the menu button
-		System.out.print(keyPressed);
-
 		if (keyPressed == true && !this.menu.getIsRunning()) 
 		{
 			this.menu.setMenuVal();
@@ -113,16 +135,24 @@ public class HoldMyHand extends Module /*implements ShapeEditorInterface */{
 
 		//		background(this.menu.getCanvasColor()[0], this.menu.getCanvasColor()[1], this.menu.getCanvasColor()[2]);
 
-		scaleDegree	= (round(input.getAdjustedFundAsMidiNote(0)) - this.menu.getCurKeyEnharmonicOffset() + 3 + 12) % 12;
-		this.menu.fadeColor(scaleDegree, 0);
-		background(this.menu.getCurHue()[0][0], this.menu.getCurHue()[0][1], this.menu.getCurHue()[0][2]);
-
-		// pick the appropriate color by checking amplitude threshold
-		float	curAmp		= this.input.getAmplitude();
+		/*
+		scaleDegree	= (round(input.getAdjustedFundAsMidiNote(this.soloistInput)) - this.menu.getCurKeyEnharmonicOffset() + 3 + 12) % 12;
+		this.menu.fadeColor(scaleDegree, this.soloistInput);
+		*/
+		if(this.menu.getRecInputPlaying())
+		{
+			scaleDegree	= (round(this.menu.getRecInput().getAdjustedFundAsMidiNote(this.soloistInput)) - this.menu.getCurKeyEnharmonicOffset() + 3 + 12) %12;
+		} else {
+			System.out.println("input check");
+			scaleDegree	= (round(input.getAdjustedFundAsMidiNote(this.soloistInput)) - this.menu.getCurKeyEnharmonicOffset() + 3 + 12) %12;
+		}
+		
+		this.menu.fadeColor(scaleDegree, this.soloistInput);		
+		background(this.menu.getCurHue()[this.soloistInput][0], this.menu.getCurHue()[this.soloistInput][1], this.menu.getCurHue()[this.soloistInput][2]);
 
 		renderWave();
 
-		for(int i=0;i<curNumInputs;i++)
+		for(int i=0;i<this.curNumInputs;i++)
 		{
 			this.menu.updateAmplitudeFollower(i,1);
 		}
@@ -134,21 +164,21 @@ public class HoldMyHand extends Module /*implements ShapeEditorInterface */{
 		noStroke();
 		fill(255);
 		// A simple way to draw the line with an ellipse at each location
-			for(int i = 0; i<curNumInputs; i++)
+			for(int i = 0; i<this.inputNums.length; i++)
 			{
 				//checks current amplitude to determine each amplitude step size for each new line 
-				amp = this.menu.getAmplitudeFollower(i);
+				amp = this.menu.getAmplitudeFollower(this.inputNums[i]);
 				ampLevel = (Math.min(amp, maxAmp)/maxAmp)*yvalues.length;
-				inputWidth = width/curNumInputs; 
-				numEllipses  = inputWidth/xspacing; 
-				System.out.println("ampLevel: "+ampLevel);
-				System.out.println("Curent Amplitute: "+amp);
-				for(int y = 0; y<ampLevel; y++)
+				
+				//System.out.println("ampLevel: "+ampLevel);
+				//System.out.println("Curent Amplitute: "+amp);
+				
+				for(int y = 0; y < ampLevel; y++)
 				{
-					for(int x = 0; x<(numEllipses); x++)
+					for(int x = 0; x < this.numEllipses; x++)
 					{
 						//fill(255,0,0);
-						ellipse(((x*xspacing)+((inputWidth)*i)), (yvalues[y]), 12, 12);//bottom line
+						ellipse(((x*xspacing)+((this.inputWidth)*i)), (yvalues[y]), 12, 12);//bottom line
 					}
 				}
 			}
--- Visuals/Eclipse/workspace/CodeAndChordsEclipseProject/src/core/ModuleMenu.java
+++ Visuals/Eclipse/workspace/CodeAndChordsEclipseProject/src/core/ModuleMenu.java
@@ -512,17 +512,24 @@ public class ModuleMenu extends MenuTemplate  {
 				"6_Part_Scale6.wav",
 				"6_Part_Scale7.wav"
 		});
+		
 		this.recInput.pause(true);
 		this.useRecInput		= false;
 		this.recInputPlaying	= false;
 		*/
-		
+	
 		this.recInput	= new RecordedInput(this.module, new String[] {
-				"cadenza/Hold My Hand - Alto.wav",
-				"cadenza/Hold My Hand - Mezzo.wav",
-				"cadenza/Hold My Hand - Soprano.wav",
-				"cadenza/Hold My Hand - Melody.wav"
+				"6_Part_Scale1.wav",
+				"6_Part_Scale2.wav",
+				"6_Part_Scale3.wav",
+				"6_Part_Scale4.wav",
+				"6_Part_Scale5.wav",
+				"6_Part_Scale6.wav",
+				"6_Part_Scale7.wav",
+				"cadenza/Here - Melody.wav",
+				"cadenza/Here - Beatbox.wav"
 		});
+		
 		this.recInput.pause(false);
 		this.useRecInput		= true;
 		this.recInputPlaying	= true;
@@ -1993,7 +2000,7 @@ public class ModuleMenu extends MenuTemplate  {
 	} // fadeColor
 	
 	/**
-	 * this method uptades the amplitude follower and needs to be called every time in the draw loop
+	 * this method updates the amplitude follower and needs to be called every time in the draw loop
 	 * for each input that is using it.
 	 * 
 	 * @param numInput:  Controls the input that is updated
@@ -2012,7 +2019,12 @@ public class ModuleMenu extends MenuTemplate  {
 		//current amplitudeFollower value, to the value returned by input.getAmplitude
 		if(followerType == 1)
 		{
-			this.amplitudeFollower[numInput] = (this.amplitudeFollower[numInput] + this.input.getAmplitude()) / 2;
+			if(!this.recInputPlaying)
+			{
+				this.amplitudeFollower[numInput] = (this.amplitudeFollower[numInput] + this.input.getAmplitude(numInput)) / 2;
+			} else {
+				this.amplitudeFollower[numInput] = (this.amplitudeFollower[numInput] + this.recInput.getAmplitude(numInput)) / 2;
+			}
 		}
 		
 		//this variation of the amplitude follower always moves the follower half way from the
@@ -2021,7 +2033,13 @@ public class ModuleMenu extends MenuTemplate  {
 		//value can change in one increment
 		if(followerType == 2)
 		{
-			float amp = this.input.getAmplitude(numInput);
+			float	amp;
+			if(!this.recInputPlaying)
+			{
+				amp = this.input.getAmplitude(numInput);
+			} else {
+				amp	= this.recInput.getAmplitude(numInput);
+			}
 			
 			if(this.maxAmplitude[numInput] < amp)
 			{
