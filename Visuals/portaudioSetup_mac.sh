46974615e39f3deb5fa40971fda061ad798a31f5
Emily Meuer <emily.meuer@gmail.com>
2018-05-22 16:04:10 -0500

    Beginnings of a PortAudio auto-compile script

---
+++ Visuals/portaudioSetup_mac.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#
+# Downloads and compiles PortAudio and JPortAudio.
+# (PortAudio compilation taken from the AVS SDK setup script:
+# https://github.com/jahianancyyang/AlarmBuddy/blob/master/amazon-skills-development/alexa-pi/fullsetup.sh)
+#
+# Step-by-step JNI compliation instructions from https://medium.com/@bschlining/a-simple-java-native-interface-jni-example-in-java-and-scala-68fdafe76f5f.
+
+PORT_AUDIO_FILE="pa_stable_v190600_20161030.tgz"
+PORT_AUDIO_DOWNLOAD_URL="http://www.portaudio.com/archives/$PORT_AUDIO_FILE"
+
+FRAMEWORKS="-framework CoreAudio -framework AudioToolbox -framework AudioUnit -framework CoreServices -framework Carbon"
+
+curl -O $PORT_AUDIO_DOWNLOAD_URL
+tar -xzf $PORT_AUDIO_FILE
+
+cd portaudio
+./configure --without-jack
+make
+
+cc -dynamiclib -o libjportaudio.jnilib hola.o hello.o $FRAMEWORKS
+
+cc -dynamiclib -O3 \
+    -I/usr/include \
+    -I$JAVA_HOME/include \
+    -I$JAVA_HOME/include/darwin \
+    ./portaudio/bindings/java/c/src/com_portaudio_BlockingStream.c \
+    ./portaudio/bindings/java/c/src/com_portaudio_PortAudio.c \
+    ./portaudio/bindings/java/c/src/com_portaudio_jpa_tools.c \
+    -o libjportaudio.dylib
